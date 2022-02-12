63b269bb8154a8cbf911526569f147f8e215f624
nikolastas <54937147+nikolastas@users.noreply.github.com>
2022-02-12 18:04:42 +0200

    lesson 15

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: NinjaCrad(),
 ));
 
-class Home extends StatelessWidget{
+class NinjaCrad extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Color.fromARGB(150, 12, 0, 179)
-    ),
-    body: Container(
-      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 30.0),
-      color: Colors.grey[400],
-      child: Text('hello'),
-       
-    ),
-    floatingActionButton: FloatingActionButton(
-      backgroundColor: Colors.red[600],
-      onPressed: () {  },
-      child: Text('click'),
-    ),
-  );
+      backgroundColor: Colors.green[900],
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/image1.jpg'),
+                radius: 50,
+              ),
+            ),
+            Divider(
+              height: 50,
+              color: Colors.green[600],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.green[200],
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox( height:10),
+            Text(
+              'Nikos',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox( height:30),
+            Text(
+              'LASTNAME',
+              style: TextStyle(
+                color: Colors.green[200],
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox( height:10),
+            Text(
+              'Tasiopoulos',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox( 
+              height:30
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.green[100],
+                  ),
+                  SizedBox( width: 10),
+                  Text(
+                    'nikos@email.com',
+                    style: TextStyle(
+                      color: Colors.green[200],
+                      fontSize: 18,
+                      letterSpacing: 1
+                    ),
+                  )
+                ],
+              )
+          ],
+        ),
+      )
+    );
   }
 }
