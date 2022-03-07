c5cb923a40da5c35de1bc232f37c3434c4fc0177
tsiakag <72927884+tsiakag@users.noreply.github.com>
2022-03-07 17:46:28 +0200

    Revert "first try"
    This reverts commit 98941bfa2d75c567ba41d6dccdc72e6422823e5b.

--- lib/Calender.dart
+++ lib/Calender.dart
@@ -1,8 +1,4 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
-import 'colors&Textlines/colorsAndTextlines.dart';
 
 // class profile extends StatefulWidget {
 //   final List? widgetOptions1;
@@ -15,7 +11,6 @@ import 'colors&Textlines/colorsAndTextlines.dart';
 //   @override
 //   _profileState createState() => _profileState();
 // }
-var _selectedDay, _focusedDay;
 
 class callendar extends StatelessWidget {
   List widgetOptions;
@@ -47,142 +42,7 @@ class callendar extends StatelessWidget {
               color: Color.fromRGBO(253, 253, 255, 1),
             )),
       ),
-      SizedBox(height: 30),
-      Container(
-        width: 392,
-        height: 340,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(23),
-            topRight: Radius.circular(23),
-            bottomLeft: Radius.circular(23),
-            bottomRight: Radius.circular(23),
-          ),
-          color: primaryWhite,
-        ),
-        child: TableCalendar(
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2030, 3, 14),
-            focusedDay: DateTime.now(),
-            calendarStyle: CalendarStyle(
-              todayDecoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(90),
-                    topRight: Radius.circular(90),
-                    bottomLeft: Radius.circular(90),
-                    bottomRight: Radius.circular(90),
-                  ),
-                  color: primaryBlue),
-              selectedDecoration:
-                  BoxDecoration(color: primaryBlue), //TODO change later
-            )),
-      ),
-      SizedBox(height: 30),
-      Container(
-        width: 388,
-        height: 153,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(23),
-            topRight: Radius.circular(23),
-            bottomLeft: Radius.circular(23),
-            bottomRight: Radius.circular(23),
-          ),
-          color: primaryPink,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30),
-            const Text('Choose Dates:'),
-            const SizedBox(height: 30),
-            Row(
-              children: [
-                SizedBox(width: 100),
-                DropdownButton<String>(
-                  value: 'one',
-                  // isExpanded: true,
-                  items: <String>['one', 'two', 'three']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: null,
-                  style: const TextStyle(color: Colors.amber),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                ),
-                SizedBox(width: 10),
-                DropdownButton<String>(
-                  value: 'das',
-                  // isExpanded: true,
-                  items: <String>['hadsa', 'das', 'thrsdasde']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: null,
-                  style: const TextStyle(color: Colors.amber),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                ),
-                SizedBox(width: 10),
-                TextButton(
-                  onPressed: null,
-                  style: TextButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 8),
-                      backgroundColor: Colors.deepPurple),
-                  child: Text('Test3'),
-                )
-              ],
-            ),
-            const SizedBox(height: 30),
-            Container(
-                width: 400,
-                height: 80,
-                alignment: Alignment.centerLeft,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(23),
-                    topRight: Radius.circular(23),
-                    bottomLeft: Radius.circular(23),
-                    bottomRight: Radius.circular(23),
-                  ),
-                  color: primaryWhite,
-                ),
-                child: Row(
-                  children: [
-                    SizedBox(width: 20),
-                    Text("On (date) you spent:"),
-                    SizedBox(width: 160),
-                    Container(
-                      height: 70,
-                      width: 70,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(90),
-                          topRight: Radius.circular(90),
-                          bottomLeft: Radius.circular(90),
-                          bottomRight: Radius.circular(90),
-                        ),
-                        color: Colors.deepPurple,
-                      ),
-                      child: Text('dolar'),
-                    )
-                  ],
-                ))
-          ],
-        ),
-      )
+      // screens.elementAt(selectedIndex)
     ]);
   }
 }
